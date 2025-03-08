e0b0cfdc9fb29337e41ea729a625903b558dd53a
Allan MacDonald <allanmacdonald@mac.home>
2025-03-08 14:12:46 -0400

    cors issue fixed

--- backend_api/main.go
+++ backend_api/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// Check if we are in development mode
+	devMode := os.Getenv("DEV_MODE") == "true"
+
+	if devMode {
+		// Allow all origins in development
+		r.Use(cors.New(cors.Config{
+			AllowOrigins:     []string{"*"},
+			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}))
+	}
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":3000") // listen and serve on 0.0.0.0:3000
+	r.Run("0.0.0.0:3000") // listen and serve on 0.0.0.0:3000
 }
--- run_backend_api.sh
+++ run_backend_api.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd backend_api
 go build -o backend_api
-./backend_api
+DEV_MODE=true ./backend_api
